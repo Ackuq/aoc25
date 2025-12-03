f246d225c9857ee6933695fea04c260db0537af4
Axel Pettersson <axel@pettersson.cc>
2025-12-03 07:28:17 +0100

    Solve day 2 using regex

--- aoc25/day2/solution.py
+++ aoc25/day2/solution.py
@@ -1,3 +1,6 @@
+import re
+
+
 def parse_input(lines: list[str]) -> list[range]:
     pairs = lines[0].split(",")
     ranges = list[range]()
@@ -8,31 +11,17 @@ def parse_input(lines: list[str]) -> list[range]:
     return ranges
 
 
-possible_splits = [2, 3, 5, 7]
+re_part1 = r"^(\d*)\1{1}$"
+re_part2 = r"^(\d*)\1+$"
 
 
 def is_invalid(num: int, part2: bool = False) -> bool:
     num_str = str(num)
 
-    if not part2:
-        if len(num_str) % 2 != 0:
-            return False
-        left_part = num_str[: len(num_str) // 2]
-        right_part = num_str[len(num_str) // 2 :]
+    if part2:
+        return re.match(re_part2, num_str) is not None
 
-        return left_part == right_part
-
-    for split in possible_splits:
-        if len(num_str) % split != 0:
-            continue
-
-        part_length = len(num_str) // split
-        parts = [num_str[i * part_length : (i + 1) * part_length] for i in range(split)]
-
-        if all(part == parts[0] for part in parts):
-            return True
-
-    return False
+    return re.match(re_part1, num_str) is not None
 
 
 def part1(ranges: list[range]) -> None:
